25a186ffcf41c90aa7d25dc2ab5a634360d99127
henritresor <shimwamanahenritresor@gmail.com>
2023-09-07 20:14:13 +0200

    done with tab 1

--- src/components/tabs/Tab1.tsx
+++ src/components/tabs/Tab1.tsx
@@ -8,7 +8,8 @@ const Tab1 = () => {
                         <div className="border-dashed border-2 p-5 gap-3 flex flex-col items-center justify-center rounded-2xl w-[40%]">
                                 <Image src="/svg/pdf.svg" alt="" width={100} height={100} />
                                 <p className='text-2xl'>Upload your PDF file</p>
-                                <button className='px-4 py-2 rounded-md bg-blue-500 text-white'>Browse</button>
+                                {/* <button className='px-4 py-2 rounded-md bg-blue-500 text-white'>Browse</button> */}
+                                <label htmlFor="file" className='px-4 py-2 rounded-md bg-blue-500 text-white block'>Browse</label>
                         </div>
                 </div>
         )
